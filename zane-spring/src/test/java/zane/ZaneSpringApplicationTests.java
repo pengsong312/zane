ce3a130b4c6cd389d3f025371188388d7af3adea
彭松 <pinePeng@outlook.com>
2022-06-29 19:02:04 +0800

    init

---
+++ zane-spring/src/test/java/zane/ZaneSpringApplicationTests.java
@@ -0,0 +1,18 @@
+package zane;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ZaneSpringApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void test(){
+        Assert.assertTrue(1 == 1);
+    }
+
+}
